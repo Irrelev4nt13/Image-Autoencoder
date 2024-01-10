42945a7f43cc4738f517bb6a60f5f4ed0c14e6c7
Michail Chatzispyrou <sdi2000212@di.uoa.gr>
2024-01-10 17:56:41 +0200

    add temp cluster test

--- tests/cluster_test.cpp
+++ tests/cluster_test.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <cstring>
 
 int main(int argc, char const *argv[])
 {
+    bool original = false;
+    for (int i = 0; i < argc; i++)
+        if (!strcmp(argv[i], "-init"))
+            original = true;
+
+    if (original)
+    {
+        ;
+        return 0;
+    }
     std::cout << "hello from cluster test" << std::endl;
     return 0;
 }
